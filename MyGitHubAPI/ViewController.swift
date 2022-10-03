cf52b3f4f8c5bb3a0146d9a8f691181ff7c75c86
AnnaSemichaeva <tdhnss@gmail.com>
2022-10-03 23:44:25 +0300

    minor changes

--- MyGitHubAPI/ViewController.swift
+++ MyGitHubAPI/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var array = ["a", "b", "c", "d", "e", "f"]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
-
-
+    
+    
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        cell?.textLabel!.text = "\(array[indexPath.row])"
+        return cell!
+    }
+}
