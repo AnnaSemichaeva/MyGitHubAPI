4e6248a8f5a4fcdfcf608e03e5b357f7892e4ec4
AnnaSemichaeva <tdhnss@gmail.com>
2022-11-13 02:56:54 +0300

    hmmmmm difficult

---
+++ MyGitHubAPI/View/ReposTableViewCell.swift
@@ -0,0 +1,162 @@
+//
+//  ReposTableViewCell.swift
+//  MyGitHubAPI
+//
+//  Created by macuser on 11/12/22.
+//
+
+import UIKit
+
+class ReposTableViewCell: UITableViewCell {
+
+    static let reuseId = "repositoryCell"
+    
+    let cardView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 20
+        view.backgroundColor = #colorLiteral(red: 0.800581634, green: 0.589300096, blue: 1, alpha: 1)
+        
+        // shadow setup
+        view.layer.shadowColor = #colorLiteral(red: 0.09200996906, green: 0.08846413344, blue: 0.1079702899, alpha: 1)
+        view.layer.shadowRadius = 3
+        view.layer.shadowOpacity = 0.2
+        view.layer.shadowOffset = CGSize(width: 5, height: 10)
+        
+        return view
+    }()
+    
+    let cardViewGradient: GradientView = {
+        let gradientView = GradientView()
+        gradientView.translatesAutoresizingMaskIntoConstraints = false
+        var startColor = Constants.cardViewTopColor
+        var endColor = Constants.cardViewBottomColor
+        gradientView.setColors(startColor: startColor, endColor: endColor)
+        gradientView.layer.cornerRadius = 20
+        gradientView.layer.masksToBounds = true
+        return gradientView
+    }()
+    
+    let repoId: PaddingLabel = {
+        var label = PaddingLabel()
+        label.setInsets(insets: UIEdgeInsets(top: 3, left: 10, bottom: 4, right: 10))
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = Constants.repoIdFont
+        label.backgroundColor = Constants.gitgubWhiteColor
+        label.textColor = #colorLiteral(red: 0.4477045536, green: 0.4294939339, blue: 0.4558002353, alpha: 1)
+        label.layer.cornerRadius = 10
+        label.layer.masksToBounds = true
+        return label
+    }()
+    let repoName: PaddingLabel = {
+        var label = PaddingLabel()
+        label.setInsets(insets: UIEdgeInsets(top: 3, left: 5, bottom: 4, right: 5))
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = Constants.repoNameFont
+        label.backgroundColor = Constants.gitgubWhiteColor
+        label.layer.cornerRadius = 10
+        label.layer.masksToBounds = true
+        
+        // add minimum scale if text is too long
+        label.numberOfLines = 1;
+        label.minimumScaleFactor = 8/label.font.pointSize;
+        label.adjustsFontSizeToFitWidth = true;
+        
+        return label
+    }()
+    let ownerName: PaddingLabel = {
+        var label = PaddingLabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = Constants.repoOwnerFont
+        label.backgroundColor = Constants.gitgubWhiteColor
+        label.layer.cornerRadius = 10
+        label.layer.masksToBounds = true
+        return label
+    }()
+    let repoDescription: UITextView = {
+        var label = UITextView()
+        
+        label.font = Constants.repoDescriptionFont
+        label.isScrollEnabled = false
+        label.isSelectable = true
+        label.isUserInteractionEnabled = true
+        label.isEditable = false
+        
+        let padding = label.textContainer.lineFragmentPadding
+        label.textContainerInset = UIEdgeInsets.init(top: 0, left: -padding, bottom: 0, right: -padding)
+        
+        label.dataDetectorTypes = UIDataDetectorTypes.all
+        
+        label.backgroundColor = .clear
+        label.textColor = Constants.gitgubWhiteColor
+        return label
+    }()
+    let repoOwnerImageView: WebImageView = {
+        let imageView = WebImageView()
+        imageView.backgroundColor = .blue
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = Constants.repoOwnerAvatarSize / 2
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        backgroundColor = .clear
+        manageConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func manageConstraints(){
+        // MARK: cardView
+        addSubview(cardView)
+        cardView.fillSuperview(padding: Constants.cardViewOffset)
+        
+        // MARK: cardView gradient
+        cardView.addSubview(cardViewGradient)
+        cardViewGradient.fillSuperview()
+        
+        // MARK: Repo ID constraints
+        cardView.addSubview(repoId)
+        repoId.topAnchor.constraint(equalTo: cardView.topAnchor, constant: Constants.repoNameInsets.top).isActive = true
+        repoId.trailingAnchor.constraint(equalTo: cardView.trailingAnchor, constant: -Constants.repoIdInsets.right).isActive = true
+        
+        // MARK: Repo name constraints
+        cardView.addSubview(repoName)
+        repoName.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: Constants.repoNameInsets.left).isActive = true
+        repoName.topAnchor.constraint(equalTo: cardView.topAnchor, constant: Constants.repoNameInsets.top).isActive = true
+        repoName.trailingAnchor.constraint(lessThanOrEqualTo: repoId.leadingAnchor, constant: -Constants.repoNameInsets.right).isActive = true
+        
+        // MARK: Repo owner Image constraints
+        cardView.addSubview(repoOwnerImageView)
+        repoOwnerImageView.heightAnchor.constraint(equalToConstant: Constants.repoOwnerAvatarSize).isActive = true
+        repoOwnerImageView.widthAnchor.constraint(equalToConstant: Constants.repoOwnerAvatarSize).isActive = true
+        repoOwnerImageView.topAnchor.constraint(equalTo: repoName.bottomAnchor, constant: Constants.repoOwnerAvatarInsets.top).isActive = true
+        repoOwnerImageView.leadingAnchor.constraint(equalTo: cardView.leadingAnchor, constant: Constants.repoOwnerAvatarInsets.left).isActive = true
+        
+        // MARK: Repo owner Name constraints
+        cardView.addSubview(ownerName)
+        ownerName.centerYAnchor.constraint(equalTo: repoOwnerImageView.centerYAnchor).isActive = true
+        ownerName.leadingAnchor.constraint(equalTo: repoOwnerImageView.trailingAnchor, constant: Constants.repoOwnerInsets.left).isActive = true
+        
+        // MARK: repo description constraints
+        // constraints set in code from view model
+        cardView.addSubview(repoDescription)
+    }
+    
+//    func setData(viewModel: TableViewCellViewModel){
+//        repoId.text = viewModel.repoId
+//        repoName.text = viewModel.repoName
+//        ownerName.text = viewModel.repoOwnerName
+//        repoDescription.text = viewModel.repoDescription
+//        repoOwnerImageView.set(imageURL: viewModel.repoOwnerAvatarUrl)
+//        
+//        // MARK: hard 'constraints' by frame
+//        repoDescription.frame = viewModel.repoCellSizes.repoDescriptionFrame
+//    }
+}
+
--- MyGitHubAPI/ViewController.swift
+++ MyGitHubAPI/ViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 class ViewController: UIViewController {
     
     var array = ["a", "b", "c", "d", "e", "f"]
+    var contentSizeSubtraction: Double!
     
     @IBOutlet weak var tableView: UITableView!
+    
+    let titleView = TitleView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.4550600524, green: 0.02154863621, blue: 0.1925193302, alpha: 1)
         
         setUpTableView()
     }
@@ -25,14 +29,50 @@ class ViewController: UIViewController {
         
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
+        tableView.backgroundColor = .green
+        
         tableView.delegate = self
         tableView.dataSource = self
     }
     
-    
+    func initialSetup(){
+        contentSizeSubtraction = view.frame.size.height + Double(100)
+        tableView.register(ReposTableViewCell.self, forCellReuseIdentifier: ReposTableViewCell.reuseId)
+        tableView.refreshControl = createRefreshControl()
+        //loadDataInitially(completion: nil)
+        
+        navigationItem.titleView = titleView
+    }
 }
+    
+//    func loadDataInitially(completion: (() -> ())?){
+//        NetworkingHelpers.decodeDataWithResult(from: viewModel.getGithubRepositoriesLink(), type: [Repository].self, printJSON: false) { [weak self] result in
+//
+//            DispatchQueue.main.async {
+//                switch result{
+//                case .success(let repositories):
+//                    self?.viewModel.setRepositories(newRepos: repositories)
+//                    self?.viewModel.lastRepoLoadedId = repositories.last?.id ?? 0
+//                    print("Number of repos: \(repositories.count)")
+//                case .failure(let error):
+//                    self?.viewModel.setRepositories(newRepos: [])
+//                    self?.viewModel.lastRepoLoadedId = 0
+//                    print("*Couldn't get data = failure: \(error.localizedDescription)")
+//                }
+//
+//                self?.tableView.reloadData()
+//                self?.viewModel.fetchingData = false
+//
+//                completion?()
+//            }
+//        }
+//    }
+//
+//}
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         array.count
     }
@@ -44,3 +84,22 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
         return cell!
     }
 }
+
+
+    // MARK: pull to refresh
+extension ViewController {
+        
+        private func createRefreshControl() -> UIRefreshControl{
+            let refreshControl = UIRefreshControl()
+            refreshControl.addTarget(self, action: #selector(onCalledToRefresh(sender:)), for: .valueChanged)
+            
+            return refreshControl
+        }
+        
+        @objc private func onCalledToRefresh(sender: UIRefreshControl){
+            print("onCalledToRefresh")
+//            loadDataInitially {
+//                sender.endRefreshing()
+//            }
+        }
+    }
